ef8100dc23ad02d819387c91d6125fcf571a2526
Guillaume Biton <guillaume.biton@free.fr>
2025-07-30 17:42:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const app = new Framework7({
   // App root element
   el: '#app',
   // App Name
-  name: 'My App',
+  name: 'Mes frais',
+  // App default theme
+  theme: auto,
   // Add default routes
   routes: [],
   // ... other parameters
