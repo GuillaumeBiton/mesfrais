3225cf0974e2890fa080e798e1a337421ca79c4b
Guillaume Biton <guillaume.biton@free.fr>
2025-07-30 18:38:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ document.getElementById('photo-input').addEventListener('change', async (event) 
     const img = new Image();
     img.src = reader.result;
     img.onload = async () => {
-      document.getElementById('preview-container').innerHTML = `<div style="background: url(${img.src}); background-size: cover;" />`;
+      document.getElementById('preview-container').innerHTML = `<img src="${img.src}" />`;
 
       const result = await Tesseract.recognize(img, 'fra');
       const text = result.data.text;
